5edfcaa17e57d13c7831a31ffb4ba72ae7cf490c
bakha0 <112553444+bakha0@users.noreply.github.com>
2022-11-02 17:27:41 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    
+    $('.header__button').on('click', function(){
+        
+        $('.header__box').toggleClass('header__box-active');
+        
+    })
+    
+    $('.header__button-cancel').on('click', function(){
+        
+        $('.header__box').toggleClass('header__box-active');
+        
+    })
+    
+})
